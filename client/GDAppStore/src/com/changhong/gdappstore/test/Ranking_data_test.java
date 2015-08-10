8f9d26e0a694bd4c645648da74c43fa689585ac6
kunkun39 <34445282@qq.com>
2015-08-10 18:12:19 +0800

    更改了测试图片的命名格式

--- client/GDAppStore/src/com/changhong/gdappstore/test/Ranking_data_test.java
+++ client/GDAppStore/src/com/changhong/gdappstore/test/Ranking_data_test.java
@@ -20,43 +20,43 @@ public class Ranking_data_test {
 		Ranking_Item item;
 		
 		item = new Ranking_Item(1, null, "植物大战僵尸", "100万+", "117.6M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.zhiwudazhan);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_zhiwudazhan);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(2, null, "酷狗音乐", "300万+", "4.7M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.kugou);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_kugou);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(3, null, "全家盒电视", "60万+", "4.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.quanjiahe);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_quanjiahe);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(4, null, "我叫MT 2", "50000+", "207.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.mt);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_mt);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(5, null, "水果忍者", "100万+", "10.2M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.shuiguo);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_shuiguo);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(6, null, "运动加加Online", "40万+", "6.3M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.yundong);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_yundong);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(7, null, "电视QQ", "100万+", "6.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.dianshiqq);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_dianshiqq);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(8, null, "我是车神", "100万+", "31.2M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.woshicheshen);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_woshicheshen);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(9, null, "开心酷跑", "100万+", "35.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.kaixinkupao);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_kaixinkupao);
 		newArrayList.add(item);
 		
 		item = new Ranking_Item(10, null, "文件管理器", "80000+", "2.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.wenjian);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_wenjian);
 		newArrayList.add(item);
 		
 		return newArrayList;
@@ -67,43 +67,43 @@ public class Ranking_data_test {
 		Ranking_Item item;
 		
 		item = new Ranking_Item(1, null, "全家盒电视", "60万+", "4.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.quanjiahe);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_quanjiahe);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(2, null, "酷狗音乐", "300万+", "4.7M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.kugou);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_kugou);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(3, null, "花样三国", "20万+", "139.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.huayangsanguo);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_huayangsanguo);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(4, null, "天天西游酷跑", "20万+", "10.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.tiantianxiyou);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_tiantianxiyou);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(5, null, "决斗吧！兄弟", "10万+", "46.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.juedouba);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_juedouba);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(6, null, "多乐助手", "30万+", "15.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.duolezhushou);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_duolezhushou);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(7, null, "Q版赛车", "10万+", "22.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.qbansaiche);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_qbansaiche);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(8, null, "雷电2015", "10万+", "14.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.leidian);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_leidian);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(9, null, "卡牌萌兽", "10万+", "39.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.kapaimengshou);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_kapaimengshou);
 		hotArrayList.add(item);
 		
 		item = new Ranking_Item(10, null, "拳皇97 高清版", "40万+", "127.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.quanhuang);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_quanhuang);
 		hotArrayList.add(item);
 		
 		return hotArrayList;
@@ -114,43 +114,43 @@ public class Ranking_data_test {
 		Ranking_Item item;
 		
 		item = new Ranking_Item(1, null, "我叫MT 2", "50000+", "207.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.mt);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_mt);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(2, null, "酷狗音乐", "300万+", "4.7M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.kugou);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_kugou);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(3, null, "全家盒电视", "60万+", "4.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.quanjiahe);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_quanjiahe);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(4, null, "电视QQ", "100万+", "6.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.dianshiqq);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_dianshiqq);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(5, null, "酷我K歌", "100万+", "14.1M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.kuwokge);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_kuwokge);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(6, null, "水果忍者", "100万+", "10.2M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.shuiguo);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_shuiguo);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(7, null, "植物大战僵尸", "100万+", "117.6M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.zhiwudazhan);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_zhiwudazhan);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(8, null, "运动加加Online", "40万+", "6.3M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.yundong);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_yundong);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(9, null, "我是车神", "100万+", "31.2M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.woshicheshen);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_woshicheshen);
 		surgeArrayList.add(item);
 		
 		item = new Ranking_Item(10, null, "电视淘宝", "30万+", "35.0M");		
-		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.taobao);
+		item.appBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_test_taobao);
 		surgeArrayList.add(item);
 		
 		return surgeArrayList;
